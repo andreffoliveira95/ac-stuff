2c1eff9f3b12d164d6a02ecf2299f9353059428f
androliv <andre.2.oliveira@nokia.com>
2023-02-18 13:23:34 +0000

    added class repo

--- implementations-ac/my-implementations/season-2/ep-04-functional/lambdas/src/org/academiadecodigo/bootcamp/lambdas/BiOperation.java
+++
@@ -1,7 +0,0 @@
-package org.academiadecodigo.bootcamp.lambdas;
-
-@FunctionalInterface
-public interface BiOperation<T> {
-
-    T execute(T operand1, T operand2);
-}
--- implementations-ac/my-implementations/season-2/ep-04-functional/lambdas/src/org/academiadecodigo/bootcamp/lambdas/MonoOperation.java
+++
@@ -1,7 +0,0 @@
-package org.academiadecodigo.bootcamp.lambdas;
-
-@FunctionalInterface
-public interface MonoOperation<T> {
-
-    T execute(T operand);
-}
